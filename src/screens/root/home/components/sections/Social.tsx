5a3f58aabdbf57f400c58bee5eaf5e12699e966b
Artem Volkov <artyom.volkow@gmail.com>
2026-05-09 15:46:30 +0300

    add social link handler

--- src/screens/root/home/components/sections/Social.tsx
+++ src/screens/root/home/components/sections/Social.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity, Linking, Alert } from 'react-native';
 import { Contact } from 'lucide-react-native/icons';
 
 import Chip from '@/components/ui/Chip';
@@ -10,37 +10,55 @@ const SocialItems = [
     label: '#LeetCode',
     color: '#f79800',
     icon: 'https://assets.leetcode.com/static_assets/public/icons/favicon.ico',
+    link: 'https://leetcode.com/u/artyomvolkov/',
   },
   {
     label: '#HackerRank',
     color: '#4ac02f',
     icon: 'https://www.hackerrank.com/favicon.ico',
+    link: 'https://www.hackerrank.com/profile/artyom_volkow',
   },
   {
     label: '#Codewars',
     color: '#f7632e',
     icon: 'https://www.codewars.com/favicon.ico',
+    link: 'https://www.codewars.com/users/Artem.Volkov',
   },
   {
     label: '#LinkedIn',
     color: '#0077B5',
+    link: 'https://www.linkedin.com/in/artem-volkov-909373109/',
   },
   {
     label: '#Github',
     color: '#333',
+    icon: 'https://github.githubassets.com/favicons/favicon.png',
+    link: 'https://github.com/ArtyomVolkov',
   },
   {
     label: '#Email',
     color: '#303030',
     icon: 'https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico',
+    link: 'mailto:artyom.volkow@gmail.com',
   },
 ];
 
 const SocialAndContactsSection = () => {
+  const handlePress = async (url: string) => {
+    try {
+      await Linking.openURL(url);
+    } catch (error) {
+      Alert.alert('Failed to open URL');
+    }
+  };
+
   return (
     <Caption icon={Contact} title="Social and Contacts">
       {SocialItems.map(item => (
-        <TouchableOpacity key={item.label}>
+        <TouchableOpacity
+          key={item.label}
+          onPress={() => handlePress(item.link)}
+        >
           <Chip
             label={item.label}
             color={item.color}
